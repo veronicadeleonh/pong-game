b44d5ee44cb2c0448de384db6cde24c35e7bd918
Veronica De Leon <v.dleon@gmail.com>
2020-08-24 12:14:45 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// const game = new Game();
+
+// function preload() {
+//   game.preloadGame();
+// }
+
+function setup() {
+  createCanvas(600, 600);
+  background("#020314");
+  // game.setupGame();
+}
+
+function draw() {
+  //  clear();
+  ellipse(50, 50, 50, 50);
+  // game.drawGame();
+}
+
+function keyPressed() {}
